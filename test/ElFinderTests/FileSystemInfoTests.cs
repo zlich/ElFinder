a1e610ab66c7d2b1824f8c48cb689cdd6f4f0ee0
Evgen Noskov <evgeny.noskov@gmail.com>
2015-04-13 23:58:37 +0300

    FileSystemInfo tests added

--- test/ElFinderTests/FileSystemInfoTests.cs
+++ test/ElFinderTests/FileSystemInfoTests.cs
@@ -32,12 +32,90 @@ namespace ElFinderTests
         [TestMethod]
         public void TestMimeType()
         {
+            string path = TestHelper.GetTestDataPath("testText.txt");
             LocalFileSystemRoot root = new LocalFileSystemRoot(TestHelper.TestDataPath);
-            LocalFileInfo info = new LocalFileInfo(root, "testText.txt");
+
+            LocalFileInfo info = new LocalFileInfo(root, path);
             Assert.AreEqual("text/plain", info.MimeType);
 
-            info = new LocalFileInfo(root, "testPicture.jpg");
+            path = TestHelper.GetTestDataPath("testPicture.jpg");
+            info = new LocalFileInfo(root, path);
             Assert.AreEqual("image/jpeg", info.MimeType);
+
+            path = TestHelper.GetTestDataPath("testText");
+            info = new LocalFileInfo(root, path);
+            Assert.AreEqual("unknown", info.MimeType);
         }
+
+        [TestMethod]
+        public void TestExtension()
+        {
+            string path = TestHelper.GetTestDataPath("testText.txt");
+            LocalFileSystemRoot root = new LocalFileSystemRoot(TestHelper.TestDataPath);
+
+            LocalFileInfo info = new LocalFileInfo(root, path);
+            Assert.AreEqual(".txt", info.Extension);
+
+            path = TestHelper.GetTestDataPath("testPicture.jpg");
+            info = new LocalFileInfo(root, path);
+            Assert.AreEqual(".jpg", info.Extension);
+
+            path = TestHelper.GetTestDataPath("testText");
+            info = new LocalFileInfo(root, path);
+            Assert.AreEqual("", info.Extension);
+        }
+
+        [TestMethod]
+        public void TestDirectory()
+        {
+            string path = TestHelper.GetTestDataPath("testText.txt");
+            LocalFileSystemRoot root = new LocalFileSystemRoot(TestHelper.TestDataPath);
+
+            LocalFileInfo info = new LocalFileInfo(root, path);
+
+            DirectoryInfo dir = new DirectoryInfo(TestHelper.TestDataPath);
+            Assert.AreEqual(dir.Name, info.Directory.Name);
+        }
+
+        [TestMethod]
+        public void TestIsHidden()
+        {
+            string path = TestHelper.GetTestDataPath("testText.txt");
+            LocalFileSystemRoot root = new LocalFileSystemRoot(TestHelper.TestDataPath);
+
+            LocalFileInfo info = new LocalFileInfo(root, path);
+            Assert.AreEqual(false, info.IsHidden);
+
+            path = TestHelper.GetTestDataPath("testText");
+            info = new LocalFileInfo(root, path);
+            Assert.AreEqual(true, info.IsHidden);
+        }
+
+        [TestMethod]
+        public void TestExists()
+        {
+            string path = TestHelper.GetTestDataPath("testText.txt");
+            LocalFileSystemRoot root = new LocalFileSystemRoot(TestHelper.TestDataPath);
+
+            LocalFileInfo info = new LocalFileInfo(root, path);
+            Assert.AreEqual(true, info.Exists);
+
+            path = TestHelper.GetTestDataPath("testNotExistFile.txt");
+            info = new LocalFileInfo(root, path);
+            Assert.AreEqual(false, info.Exists);
+        }
+
+        //[TestMethod]
+        //public void TestCopyTo()
+        //{
+        //    string path = TestHelper.GetTestDataPath("testText.txt");
+        //    LocalFileSystemRoot root = new LocalFileSystemRoot(TestHelper.TestDataPath);
+        //    LocalFileInfo info = new LocalFileInfo(root, path);
+
+        //    using (MemoryStream stream = new MemoryStream())
+        //    {
+        //        info.CopyTo(stream);
+        //    }
+        //}
     }
 }
